66540fff57c8be71148a4a12550d948651136bde
Tiliair <tu1000@students.inghamisd.org>
2024-02-06 09:26:36 -0500

    Working 2-6

--- app/src/main/java/com/example/myapplication/CreateAccountQuizActivity.java
+++ app/src/main/java/com/example/myapplication/CreateAccountQuizActivity.java
@@ -1,6 +1,11 @@
 package com.example.myapplication;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CreateAccountQuizActivity extends AppCompatActivity {
+    Spinner spinner = (Spinner) findViewById(R.id.spinner1);
+    String[] sex = {"Male","Female"};
+
 
 }
