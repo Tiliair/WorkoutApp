118dd78fadff747b0a86259b213cb061c4b6d3a6
Tiliair <tu1000@students.inghamisd.org>
2024-02-12 09:40:23 -0500

    working

--- app/src/main/java/com/example/myapplication/CreateAccountQuizActivity.java
+++ app/src/main/java/com/example/myapplication/CreateAccountQuizActivity.java
@@ -30,13 +30,31 @@ public class CreateAccountQuizActivity extends AppCompatActivity {
             }
         });
     }
-    public void changelayout2(){
+    public void changelayout2() {
         setContentView(R.layout.newaccount2);
+
+        Button NEXT = (Button) findViewById(R.id.nextBtnToNewAccount3);
+        NEXT.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                changelayout3();
+            }
+        });
 
         Button BACK = (Button) findViewById(R.id.backBtnToNewAccount1);
         BACK.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 changelayout1();
+            }
+        });
+    }
+
+    public void changelayout3(){
+        setContentView(R.layout.newaccount3);
+
+        Button BACK = (Button) findViewById(R.id.backBtnToNewAccount2);
+        BACK.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                changelayout2();
             }
         });
     }
